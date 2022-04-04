2de803e91b3e10545001b29fa966d9759c436224
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-04-04 11:40:55 -0400

    Initial commit - Initialized the project

---
+++ src/main/java/ca/sheridancollege/blockheads/RewardModuleSpringApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.blockheads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardModuleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardModuleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sheridancollege/blockheads/RewardModuleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.blockheads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardModuleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
