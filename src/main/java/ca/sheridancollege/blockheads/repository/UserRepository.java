35323c993339fb1c8f3dbb61dd438d7a7f3597af
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-04-04 13:11:18 -0400

    Add repo annotation

--- src/main/java/ca/sheridancollege/blockheads/repository/UserRepository.java
+++ src/main/java/ca/sheridancollege/blockheads/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package ca.sheridancollege.blockheads.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ca.sheridancollege.blockheads.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
