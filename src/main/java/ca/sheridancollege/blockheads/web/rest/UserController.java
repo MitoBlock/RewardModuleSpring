0c9864987b92e8b3b0d12fadb27508ec5b3b9299
CipherFly <lastkyc@gmail.com>
2022-04-07 20:35:50 -0400

    Update UserController.java

--- src/main/java/ca/sheridancollege/blockheads/web/rest/UserController.java
+++ src/main/java/ca/sheridancollege/blockheads/web/rest/UserController.java
@@ -4,6 +4,7 @@ package ca.sheridancollege.blockheads.web.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,8 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping(value = {"", "/"})
-	public User getUser(Long id) {
+	@GetMapping(value = {"{id}", "/{id}"})
+	public User getUser(@PathVariable Long id) {
 		return userService.findById(id);
 	}
 	
